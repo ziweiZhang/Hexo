0dba2b6082d919eada036eab1c51fe5a94590756
ziweiZhang <1146243152@qq.com>
2018-06-25 09:30:18 +0800

    Site updated: 2018-06-25 09:30:15

--- js/smartresize.js
+++ js/smartresize.js
@@ -1,3 +1,1 @@
-// build time:Sun Jun 24 2018 22:32:14 GMT+0800 (中国标准时间)
-+function(i,e){var n=function(i,e,n){var r;return function t(){var t=this,u=arguments;function f(){if(!n){i.apply(t,u)}r=null}if(r){clearTimeout(r)}else if(n){i.apply(t,u)}r=setTimeout(f,e||100)}};jQuery.fn[e]=function(i){return i?this.bind("resize",n(i)):this.trigger(e)}}(jQuery,"smartresize");
-//rebuild by neat 
+!function($,sr){jQuery.fn[sr]=function(fn){return fn?this.bind("resize",(func=fn,function(){var obj=this,args=arguments;timeout?clearTimeout(timeout):execAsap&&func.apply(obj,args),timeout=setTimeout(function(){execAsap||func.apply(obj,args),timeout=null},threshold||100)})):this.trigger(sr);var func,threshold,execAsap,timeout}}(jQuery,"smartresize");
--- js/totop.js
+++ js/totop.js
@@ -1,3 +1,1 @@
-// build time:Sun Jun 24 2018 22:32:14 GMT+0800 (中国标准时间)
-$(window).scroll(function(){$(window).scrollTop()>500?$("#rocket").addClass("show"):$("#rocket").removeClass("show")});$("#rocket").click(function(){$("#rocket").addClass("launch");$("html, body").animate({scrollTop:0},500,function(){$("#rocket").removeClass("show launch")});return false});
-//rebuild by neat 
+$(window).scroll(function(){500<$(window).scrollTop()?$("#rocket").addClass("show"):$("#rocket").removeClass("show")}),$("#rocket").click(function(){return $("#rocket").addClass("launch"),$("html, body").animate({scrollTop:0},500,function(){$("#rocket").removeClass("show launch")}),!1});
